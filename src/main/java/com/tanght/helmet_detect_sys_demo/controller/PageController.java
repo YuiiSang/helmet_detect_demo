8ea8342325829b59c8823fa3700b77a5ae8248e2
Yui Sun <147167953+YuiiSang@users.noreply.github.com>
2024-12-28 20:16:52 +0800

    Delete src/main/java/com/tanght/helmet_detect_sys_demo/controller directory

--- src/main/java/com/tanght/helmet_detect_sys_demo/controller/PageController.java
+++
@@ -1,9 +0,0 @@
-package com.tanght.helmet_detect_sys_demo.controller;
-
-import com.tanght.helmet_detect_sys_demo.common.result.Result;
-
-
-public interface PageController {
-
-    Result<?> getPage(int pageNum, int pageSize, String pageName, String input);
-}
